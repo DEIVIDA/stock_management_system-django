7a5f366c929a88acd5e98f3a1b41169239d1bbbb
Deivida Gaizauskaite <57774682+DEIVIDA@users.noreply.github.com>
2023-11-12 16:34:00 +0200

    Created home.html

--- stock_management_project/stock_management_project/urls.py
+++ stock_management_project/stock_management_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from stock_management import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.home, name="home"),
+    path("admin/", admin.site.urls),
 ]
