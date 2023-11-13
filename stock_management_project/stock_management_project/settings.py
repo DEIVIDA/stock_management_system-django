c107c82b1d5d5f6b51d28c09e4c652de1a04896a
Deivida Gaizauskaite <57774682+DEIVIDA@users.noreply.github.com>
2023-11-13 09:12:08 +0200

    Added crispy form

--- stock_management_project/stock_management_project/settings.py
+++ stock_management_project/stock_management_project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
     "stock_management",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
